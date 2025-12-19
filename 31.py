96649adf616af48386b55384a87e69bf56a26ce7
laxmi0156 <laxmikushwaha0156@gmail.com>
2025-12-19 06:40:06 +0530

    Update 31.py

--- 31.py
+++ 31.py
@@ -7,8 +7,9 @@ def fun(words):
    for word in words:      
       if len(word) >= 2 and word[0] == word[-1]: 
          count += 1    
-         return count 
+   print("Number of matching words:",count ) 
 word_list = ['abc', 'xyz', 'aba', '1221', 'aa', 'ghg'] 
-print("Number of matching words:", 
-fun(word_list)) 
+fun(word_list)
  
+ 
+
